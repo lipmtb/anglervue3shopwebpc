04bf1d8c65dca5eae86e0a517b7e4c0d7bc08e6d
lipmtb <littpm@163.com>
2021-09-29 14:34:31 +0800

    项目初始化
    project base dir

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,51 @@
 import { createStore } from 'vuex'
 
-export default createStore({
+
+let users={
   state: {
+    userName:""
+  },
+  getters:{
+    userName(state){
+      return state.userName;
+    }
   },
   mutations: {
+      loginUser(state,payload){
+      
+      }
   },
   actions: {
+    loginHandle(ctx,payload){
+   
+    
+    }
+  }
+}
+
+let goods={
+  state: {
+    cartLists:[]
   },
+  getters:{
+    cartLists(){
+      return state.cartLists;
+    }
+  },
+  mutations: {
+    addCart(state,payload){
+      
+    }
+  },
+  actions: {
+  }
+}
+
+
+export default createStore({
+  
   modules: {
+    users,
+    goods
   }
 })
